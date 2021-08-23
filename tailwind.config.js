7d4d3e149169fea27e4b680ae2bc01fe8a194361
adiala <david.adiala@gmail.com>
2021-08-23 15:20:59 -0300

    last updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,53 @@
 module.exports = {
   // mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './public/**/*.html',
-  './src/**/*.{js,jsx,ts,tsx,vue}',],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
   darkMode: false, // or 'media' or 'class'
-  theme: {    
+  theme: {
     fontFamily: {
-      'roboto': ['"Roboto"', 'system-ui'],
-      'lato': ['"Lato"', 'system-ui'],
-      'rubik': ['"Rubik"', 'sans-serif'],
-     },
+      roboto: ['"Roboto"', "system-ui"],
+      lato: ['"Lato"', "system-ui"],
+      rubik: ['"Rubik"', "sans-serif"],
+    },
     extend: {
-      backgroundImage: theme => ({
-        'contact': "url('public/contact.jpg')",
-        'profile': "url('public/adriana-perfil.jpg')",
-       }),
+      backgroundImage: (theme) => ({
+        contact: "url('public/contact.jpg')",
+      }),
       keyframes: {
-        'fade-in-down': {
-          '0%': {
-            opacity:'0',
-            transform: 'translateY(-10px)'
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
           },
-          '100%': {
-            opacity:'1',
-            transform: 'translateY(0)'
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
           },
-        }
+        },
       },
       animation: {
-        'fade-in-down': 'fade-in-down 0.5s ease-out'
+        "fade-in-down": "fade-in-down 0.5s ease-out",
       },
       height: {
-        '1/4': '25vh',
-        '1/2': '40vh',
-        '3/4': '75vh',
-       },
+        "1/4": "25vh",
+        "1/2": "40vh",
+        "3/4": "75vh",
+      },
       colors: {
-        background: "#fee7d7ff",
-        primary: '#f66c13',
-        secondary: '#6c2d04ff',
+        background: "#fee7d7",
+        primary: "#f66c13",
+        secondary: "#9c2d05",
       },
     },
   },
   variants: {
     extend: {
-      
-      margin: ['last'],},
+      margin: ["last"],
+    },
   },
   plugins: [],
-}
+};
