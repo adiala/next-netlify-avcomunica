ad474f04b51bb5d8fc024bf5e5afff9e194f1c18
adiala <david.adiala@gmail.com>
2021-11-03 17:44:46 -0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,26 @@
 import "tailwindcss/tailwind.css";
 import "../styles/embla.css";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-import * as ga from "../lib/ga";
+import Script from "next/script";
 
 const Application = ({ Component, pageProps }) => {
-  const router = useRouter();
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=XXXXXXXXXX`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-  return <Component {...pageProps} />;
+          gtag('config', 'G-M44WTTJHVC');
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default Application;
