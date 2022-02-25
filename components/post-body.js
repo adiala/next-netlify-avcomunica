4731cd82f94a36cba72219ffb2d15f1d97091f20
David Adiala <david.adiala@gmail.com>
2022-02-25 13:00:58 -0300

    youtube videos

--- components/post-body.js
+++ components/post-body.js
@@ -1,11 +1,24 @@
 import markdownStyles from "./markdown-styles.module.css";
 import BlockContent from "@sanity/block-content-to-react";
+import getYouTubeId from "get-youtube-id";
+import YouTube from "react-youtube";
+
+const serializers = {
+  types: {
+    youtube: ({ node }) => {
+      const { url } = node;
+      const id = getYouTubeId(url);
+      return <YouTube videoId={id} className="youtube" />;
+    },
+  },
+};
 
 export default function PostBody({ content }) {
   return (
     <div className="max-w-2xl mx-auto">
       <BlockContent
         blocks={content}
+        serializers={serializers}
         className={markdownStyles.markdown}
       />
     </div>
